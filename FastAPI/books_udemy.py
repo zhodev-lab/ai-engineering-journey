402759bd0db05bbe9e9b49f408b16f603ea41c0d
zhodev-lab <zho.dev@gmail.com>
2025-08-08 17:55:08 -0400

    get call dynamic query

--- FastAPI/books_udemy.py
+++ FastAPI/books_udemy.py
@@ -7,10 +7,18 @@ async def first_api():
     return {"message": "Hello World"}
 
 BOOKS = [
-    {'title': 'title one ', 'author': 'author one', 'category': 'science '}
+    {'title': 'title one ', 'author': 'author one', 'category': 'science '},
+    {'title': 'title two ', 'author': 'author two', 'category': 'math '}
 ]
 
 @app.get("/books")
 async def read_all_books():
-    return BOOKS
+    return BOOKS    
 
+@app.get("/books{dynamic_param}")
+async def read_all_books(dynamic_param):
+    res = []
+    for item in BOOKS:
+        if item['title'].find(dynamic_param) >=0 or item['author'].find(dynamic_param) >=0:
+            res.append(item)
+    return { 'books' : res }
