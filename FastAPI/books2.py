950c550872602671e827cde8fd77bd55b0655972
zhodev-lab <zho.dev@gmail.com>
2025-08-10 18:24:59 -0400

    Add book creation endpoint and request model

--- FastAPI/books2.py
+++ FastAPI/books2.py
@@ -1,13 +1,7 @@
+from typing import Optional
 from fastapi import FastAPI
-
+from pydantic import BaseModel, Field
 app = FastAPI()
-
-BOOKS = []
-
-@app.get("/books")
-async def read_all_books():
-    return BOOKS
-
 
 class Book:
     id:int
@@ -22,7 +16,25 @@ class Book:
         self.author = author
         self.description = description
         self.rating = rating
-    
+
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length= 1, max_length=260)
+    rating: float = Field(gt=0, lt=6)
+
+    model_config = {
+        "JSON_schema_extra":{
+            "example": {
+                "title": "new book title",
+                "author": "who is Author",
+                "description": "description this book",
+                "rating": "1~5 number "
+            }
+        }
+    }
+
 
 BOOKS=[
     Book(1, "How to Use FastApi", "Tom", "it is a good book, very nice with FastAPI.", 4.5),
@@ -32,3 +44,18 @@ BOOKS=[
     Book(5, "Think in Java", "Ross", "thinking in Java, use Java to create any kind of project ", 5),
     Book(6, "Think in GoLang", "Peter", "Golang is super fast with micro-servers and web application", 4.5)
 ]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+    # return "Successful Create Book"
+
+def find_book_id(book: Book):
+    book.id= 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
